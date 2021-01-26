ecd4c29674916e9cfe257f8f66b0043f516b6273
mhans003 <michaeledwardhanson@gmail.com>
2021-01-26 13:55:58 -0800

    In Home.js page, added try/catch block to check for errors in api call to get all ideas from DB.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,12 +10,16 @@ const Home = () => {
 
     useEffect(() => {
         const fetchData = async () => {
+          try {
             const res = await fetch('/api/users');
-            const data = await res.json();
+            const jsonData = await res.json();
             //Sort the array by createdAt property ordered by descending values.
-            const orderData = data.sort((a, b) => (a.createdAt < b.createdAt) ? 1 : -1);
-            setIdeas(orderData);
+            const data = jsonData.sort((a, b) => (a.createdAt < b.createdAt) ? 1 : -1);
+            setIdeas([...data]);
             setIsLoaded(true);
+          } catch (error) {
+            console.log(error);
+          }
         }
         fetchData();
     }, []);
