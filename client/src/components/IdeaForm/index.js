50914606d891a9ada7146b6670977aa69858210b
mhans003 <michaeledwardhanson@gmail.com>
2021-01-26 18:31:19 -0800

    Idea form now provies a file input to upload an image. An upload button can be clicked to add this file to the idea post. When the file is successfully uploaded to an s3 bucket, the metadata returned back is used to create a key-value pair which is appended to the formData which will be used to submit the image link along with the rest of the data to the database.

--- client/src/components/IdeaForm/index.js
+++ client/src/components/IdeaForm/index.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 const IdeaForm = () => {
     //Set up state for an idea and the username of the poster.
     const [formState, setFormState] = useState({ username: '', idea: '' });
     const [characterCount, setCharacterCount] = useState(0);
+    const fileInput = useRef(null);
 
     //Update state based on form input changes.
     const handleChange = event => {
@@ -11,6 +12,38 @@ const IdeaForm = () => {
             setFormState({ ...formState, [event.target.name]: event.target.value });
             setCharacterCount(event.target.value.length);
         }
+    };
+
+    //Upload the image being uploaded.
+    const handleImageUpload = event => {
+        event.preventDefault();
+        const data = new FormData();
+        //Add the file currently selected to the data object created with the FormData class.
+        //data will now hold 'image' as a name and the file as the value.
+        data.append('image', fileInput.current.files[0]);
+        //Send image file to endpoint with the postImage function
+        const postImage = async () => {
+            try {
+                //Try sending the image to be uploaded.
+                const res = await fetch('/api/image-upload', {
+                    mode: 'cors',
+                    method: 'POST',
+                    body: data
+                })
+                if (!res.ok) throw new Error(res.statusText);
+
+                //Wait for the image metadata to be returned back from a successful server call.
+                const postResponse = await res.json();
+                //Add the image URL (returned back in the Location property of the response) to the form state to be sent.
+                //This will provide reference to the actual uploaded file in the s3 bucket.
+                setFormState({...formState, image: postResponse.Location})
+                
+                return postResponse.Location;
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        postImage();
     };
 
     //Handle form submit.
@@ -59,6 +92,21 @@ const IdeaForm = () => {
                     className="form-input col-12 col-md-9"
                     onChange={handleChange}
                 ></textarea>
+                <label className="form-input col-12  p-1">
+                    Include Image: 
+                    <input
+                        type="file"
+                        ref={fileInput}
+                        className="form-input p-2"
+                    />
+                    <button 
+                        className="btn" 
+                        onClick={handleImageUpload} 
+                        type="submit"
+                    >
+                        Upload
+                    </button>
+                </label>
                 <button className="btn col-12 col-md-3" type="submit">
                     Submit
                 </button>
